2cff7a5014ae4831085f18ea215e579245f84972
dineshpatilcloud <66240036+dineshpatilcloud@users.noreply.github.com>
2023-10-28 10:52:39 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "function" {
+    type = string
+}
+
+variable "project" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "region" {
+    type = string
+    default = "West US"
+}
